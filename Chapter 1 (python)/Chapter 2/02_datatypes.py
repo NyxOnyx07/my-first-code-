26966aa881a296aacfe8fc847f6084836960edd0
Prajwal Deshanur <prajwaldeshanur@gmail.com>
2026-02-26 09:19:31 +0530

    Fix formatting issues in 02_datatypes.py

--- Chapter 1 (python)/Chapter 2/02_datatypes.py
+++ Chapter 1 (python)/Chapter 2/02_datatypes.py
@@ -2,11 +2,12 @@ a = 1 # a is an integer
 
 b = 5.3 # b is an floating point number
 
-c = "pwajwal# c is a string
+c = "pwajwal" # c is a string
 
 d = True # d is a boolean variable 
 
 e = None # e is a NoneType variable 
+
 
 
 
